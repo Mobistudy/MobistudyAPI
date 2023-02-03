6d24984813e652ea3e7e2bbc3eb61596584752f4
Dario Salvi <dariosalvi78@gmail.com>
2023-02-03 14:14:29 +0100

    renaming of controllers

--- src/routes.mjs
+++ src/routes.mjs
@@ -1,16 +1,17 @@
 import passport from 'passport'
 
-import tasksResults from './controllers/tasksResults.mjs'
+import tasksResultsCtrl from './controllers/tasksResultsCtrl.mjs'
 import studiesStatsCtrl from './controllers/tasksResultsCtrl.mjs'
 
 const API_PREFIX = '/api'
 
 // function that sets up the routes
 export default async function (app) {
-  await tasksResults.init()
+  await tasksResultsCtrl.init()
 
-  app.get(API_PREFIX + '/tasksResults', passport.authenticate('jwt', { session: false }), tasksResults.getAll.bind(tasksResults))
-  app.post(API_PREFIX + '/tasksResults', passport.authenticate('jwt', { session: false }), tasksResults.createNew.bind(tasksResults))
+  app.get(API_PREFIX + '/tasksResults', passport.authenticate('jwt', { session: false }), tasksResultsCtrl.getAll.bind(tasksResultsCtrl))
+  app.post(API_PREFIX + '/tasksResults', passport.authenticate('jwt', { session: false }), tasksResultsCtrl.createNew.bind(tasksResultsCtrl))
+
   await studiesStatsCtrl.init()
   app.post(API_PREFIX + '/studyStats/:study_key', passport.authenticate('jwt', { session: false }), studiesStatsCtrl.createNew.bind(studiesStatsCtrl))
 }
