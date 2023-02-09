836319953e3a7698d3a272129c5501bc888db095
Dario Salvi <dariosalvi78@gmail.com>
2023-02-09 16:20:32 +0100

    adding vocabulary tests back

---
+++ test/jasmine/vocabulary.spec.js
@@ -0,0 +1,25 @@
+import { getTerm } from '../../src/services/vocabulary.mjs'
+
+describe('when searching for a medical term', () => {
+
+  it('you can retrieve heart failure in english', async () => {
+    let concepts = await getTerm('heart failure', 'disorder', 'en', 10)
+    expect(concepts.length).not.toBe(0)
+    expect(concepts).toContain({
+      term: 'Heart failure',
+      conceptId: '84114007',
+      vocabulary: 'SNOMEDCT'
+    })
+  })
+
+  it('you can retrieve heart failure in swedish', async () => {
+    let concepts = await getTerm('hjärt', 'disorder', 'sv', 10)
+    expect(concepts.length).not.toBe(0)
+    expect(concepts).toContain({
+      term: 'hjärtsvikt',
+      conceptId: '84114007',
+      vocabulary: 'SNOMEDCT-SE'
+    })
+  })
+
+})
