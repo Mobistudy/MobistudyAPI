9ae42843565aa4f77a80cad96c516ff948f00ca1
Dario Salvi <dariosalvi78@gmail.com>
2023-06-02 23:28:33 +0200

    drafting endpoint for retrieiving a single attachment

--- src/routes.mjs
+++ src/routes.mjs
@@ -2,6 +2,7 @@ import passport from 'passport'
 
 import usersCtrl from './controllers/usersCtrl.mjs'
 import tasksResultsCtrl from './controllers/tasksResultsCtrl.mjs'
+import attachmentsCtrl from './controllers/attachmentsCtrl.mjs'
 import studyStatsCtrl from './controllers/studyStatsCtrl.mjs'
 
 const API_PREFIX = '/api'
@@ -10,7 +11,6 @@ const API_PREFIX = '/api'
 export default async function (app) {
 
   await usersCtrl.init()
-
   app.post(API_PREFIX + '/login', passport.authenticate('local', { session: false }), usersCtrl.login.bind(usersCtrl))
   app.post(API_PREFIX + '/sendResetPasswordEmail', usersCtrl.sendPasswordResetEmail.bind(usersCtrl))
   app.post(API_PREFIX + '/resetPassword', usersCtrl.resetPassword.bind(usersCtrl))
@@ -23,9 +23,11 @@ export default async function (app) {
   app.delete(API_PREFIX + '/users/:user_key', passport.authenticate('local', { session: false }), usersCtrl.removeUser.bind(usersCtrl))
 
   await tasksResultsCtrl.init()
-
   app.get(API_PREFIX + '/tasksResults', passport.authenticate('jwt', { session: false }), tasksResultsCtrl.getAll.bind(tasksResultsCtrl))
   app.post(API_PREFIX + '/tasksResults', passport.authenticate('jwt', { session: false }), tasksResultsCtrl.createNew.bind(tasksResultsCtrl))
+
+  await attachmentsCtrl.init()
+  app.post(API_PREFIX + '/tasksResults/attachments/:studyKey/:userKey/:taskId/:fileName', passport.authenticate('jwt', { session: false }), attachmentsCtrl.getAttachment.bind(attachmentsCtrl))
 
   await studyStatsCtrl.init()
   app.get(API_PREFIX + '/studyStats', passport.authenticate('jwt', { session: false }), studyStatsCtrl.getLastTasksSummary.bind(studyStatsCtrl))
