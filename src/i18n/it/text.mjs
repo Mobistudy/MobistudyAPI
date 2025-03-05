300abf6298b5df05037b8858b26b6fa907791077
Rosa Sicilia <r.sicilia@unicampus.it>
2025-03-05 13:22:51 +0100

    Italian translation

---
+++ src/i18n/it/text.mjs
@@ -0,0 +1,24 @@
+export default {
+  studyStatusUpdate: {
+    studyAcceptedTitle: 'Conferma di accettazione dello studio { studyTitle }',
+    studyAcceptedThanks: 'Grazie per aver accettato di partecipare allo studio { studyTitle }.',
+    studyAcceptedConsentedTasks: 'Hai acconsentito ai seguenti compiti:',
+    studyAcceptedNotConsentedTasks: 'Non hai acconsentito ai seguenti compiti:',
+    studyCompletedTitle: 'Completamento dello studio { studyTitle }',
+    studyCompletedThanks: 'Lo studio { studyTitle } è stato completato. Grazie per la tua partecipazione.',
+    studyWithdrawnTitle: 'Ritiro dallo studio { studyTitle }',
+    studyWithdrawnThanks: 'Ti sei ritirato dallo studio { studyTitle }. Grazie per il tuo tempo.'
+  },
+  account: {
+    registrationTitle: 'Conferma di registrazione a Mobistudy',
+    registrationContent: '<p>Sei stato registrato con successo su Mobistudy.</p><p>Se non riconosci questo messaggio, rispondi a questa email e chiedici di eliminare il tuo account.</p>',
+    passwordRecoveryTitle: 'Recupero password di Mobistudy',
+    passwordRecoveryContent: `<p>Hai richiesto di reimpostare la tua password su Mobistudy.</p>
+    <p>Per favore vai a <a href={serverlink}>questa pagina web</a> per impostare una nuova password.</p>
+    <p>Oppure copia/incolla il seguente codice nell'app: {token}</p>
+    <p>Questo codice scadrà dopo 24 ore.</p>`,
+    newPasswordTitle: 'Nuova password su Mobistudy',
+    newPasswordContent: `<p>Hai impostato una nuova password su Mobistudy.</p>
+    <p>Se non riconosci questa azione, contatta immediatamente mobistudy@mau.se.</p>`
+  }
+}
