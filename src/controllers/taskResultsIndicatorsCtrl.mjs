6139117b39f93fee045bbdf9db23816ec9e26759
dariosalvi78 <dariosalvi78@gmail.com>
2026-02-16 23:11:56 +0100

    adding controller for running indicators

--- src/controllers/taskResultsIndicatorsCtrl.mjs
+++ src/controllers/taskResultsIndicatorsCtrl.mjs
@@ -3,6 +3,14 @@
  */
 import { DAL } from "../DAL/DAL.mjs"
 import { applogger } from "../services/logger.mjs"
+import jstyleActivityDailyStats from "../taskResultsIndicators/jstyleActivityDailyStats.mjs"
+import jstyleSleepDailyStats from "../taskResultsIndicators/jstyleSleepDailyStats.mjs"
+
+/**
+ * Import types from the datamodels.
+ * @typedef {import("../../models/jsdocs.js").TaskResults} TaskResults
+ * @typedef {import("../../models/jsdocs.js").TaskResultIndicators} TaskResultIndicators
+ */
 
 export default {
   /**
@@ -67,5 +75,47 @@ export default {
       applogger.error({ error: err }, "Cannot retrieve participants")
       res.sendStatus(500)
     }
+  },
+
+  /**
+   * Returns the list of available producers for task results indicators.
+   * @param {Object} req
+   * @param {Object} res
+   */
+  getTaskResultsIndicatorsProducers(req, res) {
+    res.status(200).json([
+      jstyleActivityDailyStats.producerName,
+      jstyleSleepDailyStats.producerName
+    ])
+  },
+
+  /**
+   * Launches the processing of task results indicators for a given producer, study and taskId.
+   * @param {Object} req - the request object, must include param: producer, studyKey, and taskId.
+   * @param {Object} res - the response object
+   */
+  async runTaskResultsIndicatorsProducer(req, res) {
+    // check if the user is an admin
+    if (req.user.role !== "admin") {
+      return res.status(403).send("Only admins can run producers")
+    }
+    // check parameters in request
+    const { producer, studyKey, taskId } = req.params
+    if (!producer || !studyKey || !taskId) {
+      return res.status(400).send("Missing required parameters: producer, studyKey, taskId")
+    }
+
+    // producers work for each participant, so we need to get all participants in a given study and run the producer for each of them
+    const participants = await DAL.getAllParticipants(null, studyKey)
+    for (const participant of participants) {
+      if (producer === jstyleActivityDailyStats.producerName) {
+        await jstyleActivityDailyStats.processJStyleDailyStats(studyKey, participant._key, [parseInt(taskId)])
+      } else if (producer === jstyleSleepDailyStats.producerName) {
+        await jstyleSleepDailyStats.processJStyleSleepDailyStats(studyKey, participant._key, [parseInt(taskId)])
+      } else {
+        return res.status(400).send("Unknown producer")
+      }
+    }
+    return res.status(200).send("Producer run successfully")
   }
 };
--- test/jasmine/taskResultsIndicatorsCtrl.spec.js
+++ test/jasmine/taskResultsIndicatorsCtrl.spec.js
@@ -1,6 +1,7 @@
 import taskResultsIndicatorsCtrl from '../../src/controllers/taskResultsIndicatorsCtrl.mjs'
 import { DAL } from '../../src/DAL/DAL.mjs'
 import { applogger } from '../../src/services/logger.mjs'
+import jstyleActivityDailyStats from "../../src/taskResultsIndicators/jstyleActivityDailyStats.mjs"
 import { MockResponse } from '../mocks/MockResponse.mjs'
 
 describe('Testing task results indicators controller,', () => {
@@ -90,5 +91,58 @@ describe('Testing task results indicators controller,', () => {
     expect(res.code).toBe(200)
     expect(res.data.length).toBe(2)
     expect(DAL.getAllTaskIndicators).toHaveBeenCalledWith('1978', '3344', undefined, null, null, undefined, undefined, null)
+  })
+
+  it('admin can get producers names', async () => {
+
+    let res = new MockResponse()
+    await taskResultsIndicatorsCtrl.getTaskResultsIndicatorsProducers({
+      user: {
+        role: 'researcher',
+        _key: '998877'
+      }
+    }, res)
+
+    expect(res.code).toBe(200)
+    expect(res.data.length).toBe(2)
+    expect(res.data).toContain('jstyle-activity-daily-stats')
+    expect(res.data).toContain('jstyle-sleep-daily-stats')
+  })
+
+    it('non admin cannot run producers', async () => {
+    let res = new MockResponse()
+    await taskResultsIndicatorsCtrl.runTaskResultsIndicatorsProducer({
+      user: {
+        role: 'researcher',
+        _key: '998877'
+      }
+    }, res)
+
+    expect(res.code).toBe(403)
+  })
+
+  it('admin can run producers', async () => {
+    spyOn(DAL, 'getAllParticipants').and.returnValue([
+      {
+        _key: 'participant1'
+      }
+    ])
+    spyOn(jstyleActivityDailyStats, 'processJStyleDailyStats').and.returnValue(null)
+
+    let res = new MockResponse()
+    await taskResultsIndicatorsCtrl.runTaskResultsIndicatorsProducer({
+      user: {
+        role: 'admin',
+        _key: '998877'
+      },
+      params: {
+        producer: 'jstyle-activity-daily-stats',
+        studyKey: '1978',
+        taskId: '1'
+      },
+    }, res)
+
+    expect(res.code).toBe(200)
+    expect(jstyleActivityDailyStats.processJStyleDailyStats).toHaveBeenCalledWith('1978', 'participant1', [1])
   })
 })
